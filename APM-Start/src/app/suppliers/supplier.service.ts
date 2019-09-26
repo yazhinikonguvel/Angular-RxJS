e28ce97c7d211b83b81d486b9968e977b5731ed3
Yazhini Konguvel <yazhini@Yazhinis-MacBook-Air.local>
2019-09-25 21:01:02 -0500

    higher order mapping operators

--- APM-Start/src/app/suppliers/supplier.service.ts
+++ APM-Start/src/app/suppliers/supplier.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import { throwError } from 'rxjs';
+import { throwError, of } from 'rxjs';
+import { Supplier } from './supplier';
+import { map, tap, concatMap, mergeMap, switchMap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -9,7 +11,36 @@ import { throwError } from 'rxjs';
 export class SupplierService {
   suppliersUrl = 'api/suppliers';
 
-  constructor(private http: HttpClient) { }
+  suppliersWithConcatMap$ = of(1, 5, 8)
+    .pipe(
+      tap(id => console.log('concatMap source Observble', id)),
+      concatMap(id => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`))
+    );
+
+    suppliersWithMergeMap$ = of(1, 5, 8)
+    .pipe(
+      tap(id => console.log('mergeMap source Observble', id)),
+      mergeMap(id => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`))
+    );
+    
+    suppliersWithSwitchMap$ = of(1, 5, 8)
+    .pipe(
+      tap(id => console.log('switchMap source Observble', id)),
+      switchMap(id => this.http.get<Supplier>(`${this.suppliersUrl}/${id}`))
+    );
+
+   constructor(private http: HttpClient) { 
+  //   this.suppliersWithMap$
+  //   .subscribe(o => o.subscribe(
+  //     item => console.log('map result', item)
+  //   ));
+
+  // this.suppliersWithConcatMap$.subscribe(item => console.log('concatMap result', item))
+  // this.suppliersWithMergeMap$.subscribe(item => console.log('mergeMap result', item))
+  // this.suppliersWithSwitchMap$.subscribe(item => console.log('switchMap result', item))
+
+
+  }
 
   private handleError(err: any) {
     // in a real world app, we may send the server to some remote logging infrastructure
